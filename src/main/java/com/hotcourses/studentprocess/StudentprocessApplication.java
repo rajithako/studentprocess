231557a5911172351f66c7199f62047796dace3c
rajitha.kora@gmail.com <rajitha.kora@gmail.com>
2021-07-09 11:22:28 +0530

    springboot changes

---
+++ src/main/java/com/hotcourses/studentprocess/StudentprocessApplication.java
@@ -0,0 +1,13 @@
+package com.hotcourses.studentprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentprocessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotcourses/studentprocess/StudentprocessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotcourses.studentprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentprocessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
